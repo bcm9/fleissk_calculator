a896a73f3bea24cdcb6fa34cd59a18ce602b788a
bcm9 <59711028+bcm9@users.noreply.github.com>
2022-04-14 13:12:07 -0400

    Update fleissk_calculator.m

--- fleissk_calculator.m
+++ fleissk_calculator.m
@@ -7,7 +7,7 @@ function [fleissk] = fleissk_calculator(n,N,k,k_table)
 %   k_table = N x k matrix
 % example from https://en.wikipedia.org/wiki/Fleiss%27_kappa:
 % k_table=[0 0 0 0 14;0 2 6 4 2; 0 0 3 5 6; 0 3 9 2 0; 2 2 8 1 1;7 7 0 0 0; 3 2 6 3 0; 2 5 3 2 2; 6 5 2 1 0 ;0 2 2 3 7;];
-% fleissk(14,10,5,k_table)
+% [fleissk]=fleissk_calculator(14,10,5,k_table)
 
 % pj
 p_j=sum(k_table)./sum(sum(k_table));
